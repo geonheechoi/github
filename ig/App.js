c88374af98d40a7a93cab61e573ab8a27dfdcf78
geonheechoi <58959798+geonheechoi@users.noreply.github.com>
2023-11-01 18:25:33 -0700

    jkdshfjkds

--- ig/App.js
+++ ig/App.js
@@ -9,7 +9,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text>Universal React with Expo</Text>
+      <Text>Universal React with Expso</Text>
     </View>
   );
 }
